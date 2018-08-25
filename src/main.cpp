bde84c476e35b66eeecadeadc4e2887012a0fba7
Kani-Desktop <kanishkegb@gwu.edu>
2018-08-24 22:53:42 -0400

    added basic video open

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
+
+
+int main(int argc, char **argv)
+{
+    int wait_time = 10;
+    cv::VideoCapture in_video;
+    in_video.open(0);
+
+    while (in_video.grab()) 
+    {
+        cv::Mat image, image_copy;
+        in_video.retrieve(image);
+        image.copyTo(image_copy);
+
+        cv::imshow("Camers View", image_copy);
+        char key = (char) cv::waitKey(wait_time);
+        if (key == 27)
+            break;
+    }
+
+    in_video.release();
+}
